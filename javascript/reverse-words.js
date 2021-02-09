b15cfa15ed38e65d636e6bff1b6ace4ab79e6af9
Travis Meyer <tmeyer@sciusa.com>
2021-02-09 10:53:23 -0600

    Solved reverse words

---
+++ javascript/reverse-words.js
@@ -0,0 +1,11 @@
+/*
+  https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/javascript
+  Complete the function that accepts a string parameter, and reverses each word
+  in the string. All spaces in the string should be retained.
+*/
+
+let reverseString = str => {
+  let split = str.split(' ');
+
+  return split.map(word => word.split('').reverse().join('')).join(' ');
+}
